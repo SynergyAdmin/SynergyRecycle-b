e94084e08919b724a9188d86cecb68d3561b4143
SynergyAdmin <synergyrecycle.b@gmail.com>
2025-02-21 00:31:51 +0100

    Update main.js

--- assets/css/js/main.js
+++ assets/css/js/main.js
@@ -1,1 +1,24 @@
-main.js
+// Caricamento Componenti
+function loadComponents() {
+    fetch('components/header.html')
+        .then(response => response.text())
+        .then(data => document.body.insertAdjacentHTML('afterbegin', data));
+    
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => document.body.insertAdjacentHTML('beforeend', data));
+}
+
+// Gestione Form
+document.querySelectorAll('form').forEach(form => {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        // Logica di invio
+    });
+});
+
+// Inizializzazione
+document.addEventListener('DOMContentLoaded', () => {
+    loadComponents();
+    // Altre inizializzazioni
+});
